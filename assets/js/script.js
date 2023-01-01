cb4ca59a99c569e9d72f08b74cfc193e72c91396
Oran-123 <orandillon123@gmail.com>
2023-01-01 20:15:50 +0000

    updateh the js function to display quiz content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let howToPlayCheck = document.getElementById("how-to-play-check");
 let startBtn = document.getElementById("btn--start-quiz")
-let quizContent = document.getElementsByClassName("quiz-container")
+let quizContent = document.getElementById("quiz-container")
 
 /* function inspired by https://sebhastian.com/javascript-show-hide-div-onclick-toggle/#:~:text=To%20display%20or%20hide%20a,which%20is%20block%20)%20to%20none%20.*/
 
@@ -10,15 +10,11 @@ let quizContent = document.getElementsByClassName("quiz-container")
  */
 startBtn.onclick = function (){
     if (howToPlayCheck.style.display !== "none"){
+        quizContent.style.display = "flex"
         howToPlayCheck.style.display = "none";
     } else {
+        quizContent.style.display = "none"
         howToPlayCheck.style.display="flex";
     }
-    if (howToPlayCheck.style.display == "none"){
-        quizContent.style.display = "flex"
-    } else {
-        quizContent.style.display = "none"
-    }
     console.log("start quiz button clicked")
-}
-
+    }
