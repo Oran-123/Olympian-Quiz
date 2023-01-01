2845a725b12802772aeefc8c97bb0734f612a705
Oran-123 <orandillon123@gmail.com>
2023-01-01 19:45:38 +0000

    update start quiz function to show quiz content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 let howToPlayCheck = document.getElementById("how-to-play-check");
 let startBtn = document.getElementById("btn--start-quiz")
+let quizContent = document.getElementsByClassName("quiz-container")
 
 /* function inspired by https://sebhastian.com/javascript-show-hide-div-onclick-toggle/#:~:text=To%20display%20or%20hide%20a,which%20is%20block%20)%20to%20none%20.*/
- 
+
 /**
  * Hides the div which asks the player if they understand the rules
  * once the player clicks on the button start quiz
@@ -10,8 +11,10 @@ let startBtn = document.getElementById("btn--start-quiz")
 startBtn.onclick = function (){
     if (howToPlayCheck.style.display !== "none"){
         howToPlayCheck.style.display = "none"
+        quizContent.style.display = "flex"
     } else {
         howToPlayCheck.style.display="flex"
+        quizContent.style.display = "none"
     }
     console.log("start quiz button clicked")
 }
