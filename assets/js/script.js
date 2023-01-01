a43b0a1fa0aa9303e88eb3735b4e68722b5c9d4b
Oran-123 <orandillon123@gmail.com>
2023-01-01 19:24:25 +0000

    add note to the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 let howToPlayCheck = document.getElementById("how-to-play-check");
 let startBtn = document.getElementById("btn--start-quiz")
 
-
+/* function inspired by https://sebhastian.com/javascript-show-hide-div-onclick-toggle/#:~:text=To%20display%20or%20hide%20a,which%20is%20block%20)%20to%20none%20.*/
+ 
 /**
  * Hides the div which asks the player if they understand the rules
  * once the player clicks on the button start quiz
