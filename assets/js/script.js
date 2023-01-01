5145a08099fcbf47adc1d4a5c4d39c7dbb1e5981
Oran-123 <orandillon123@gmail.com>
2023-01-01 19:17:35 +0000

    add a description to a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let howToPlayCheck = document.getElementById("how-to-play-check");
 let startBtn = document.getElementById("btn--start-quiz")
 
+
+/**
+ * Hides the div which asks the player if they understand the rules
+ * once the player clicks on the button start quiz
+ */
 startBtn.onclick = function (){
     if (howToPlayCheck.style.display !== "none"){
         howToPlayCheck.style.display = "none"
