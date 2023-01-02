d35bc8ad2d7c0ee5f33d31b3fb19b33a7924a22b
Oran-123 <orandillon123@gmail.com>
2023-01-02 13:46:40 +0000

    set the startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,19 @@ let howToPlayCheck = document.getElementById("how-to-play-check");
 let startBtn = document.getElementById("btn--start-quiz")
 let quizContent = document.getElementById("quiz-container")
 
+
+startBtn.addEventListener("click",startGame)
 /* function inspired by https://sebhastian.com/javascript-show-hide-div-onclick-toggle/#:~:text=To%20display%20or%20hide%20a,which%20is%20block%20)%20to%20none%20.*/
 
 /**
  * Hides the div which asks the player if they understand the rules
  * once the player clicks on the button start quiz
  */
-startBtn.onclick = function (){
-    if (howToPlayCheck.style.display !== "none"){
+    function startGame() {
         quizContent.style.display = "flex"
         howToPlayCheck.style.display = "none";
-    } else {
-        quizContent.style.display = "none"
-        howToPlayCheck.style.display="flex";
-    }
-    console.log("start quiz button clicked")
-    }
-
-    start
-
-    function startGame() {
-
-    }
+        console.log("start quiz button clicked")
+        }
 
     function setNextQuestion() {
 
