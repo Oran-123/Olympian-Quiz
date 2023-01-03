93a851aa529b4819f0316fdde429833bf39fdd25
Oran-123 <orandillon123@gmail.com>
2023-01-03 17:11:10 +0000

    update variable

--- assets/js/homesript.js
+++ assets/js/homesript.js
@@ -1,4 +1,4 @@
-const openModalButton = document.getElementById("button-open-rules-modal")
+const openModalButton = document.getElementById("btn--how-to-play")
 const closeModalButton = document.getElementById("button-close-rules-modal")
 const rulesModal = document.getElementById("rules-modal")
 
