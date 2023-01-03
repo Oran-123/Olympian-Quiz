aa2986c157f316304c7af7f220681638d22712ef
Oran-123 <orandillon123@gmail.com>
2023-01-03 18:18:09 +0000

    add notes

--- assets/js/howtoplaymodal.js
+++ assets/js/howtoplaymodal.js
@@ -2,10 +2,20 @@ const openModalButton = document.getElementById("btn--how-to-play")
 const closeModalButton = document.getElementById("button-close-rules-modal")
 const rulesModal = document.getElementById("rules-modal")
 
+/* the following video was used as inspiration for the modal functions https://www.youtube.com/watch?v=TAB_v6yBXIE*/ 
+
+/**
+ * Opens a modal with instructions for the quiz,
+ * when the how to play button is clicked 
+ */
 openModalButton.addEventListener("click", () => { 
     rulesModal.showModal()  
 })
 
+/**
+ * Closes the modal with instructions for the quiz,
+ * when the X button is clicked 
+ */
 closeModalButton.addEventListener("click", () => {
     rulesModal.close() 
 })
