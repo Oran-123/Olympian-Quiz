8ba841fe782a069f49d9af5710db7333d5e34dfd
Oran-123 <orandillon123@gmail.com>
2023-01-03 17:09:44 +0000

    set function to open modal

---
+++ assets/js/homesript.js
@@ -0,0 +1,11 @@
+const openModalButton = document.getElementById("button-open-rules-modal")
+const closeModalButton = document.getElementById("button-close-rules-modal")
+const rulesModal = document.getElementById("rules-modal")
+
+openModalButton.addEventListener("click", () => { 
+    rulesModal.showModal()  
+})
+
+closeModalButton.addEventListener("click", () => {
+    rulesModal.closeModal() 
+})
