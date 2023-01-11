64b0158e22263ab13306d1a34bbc886a0c3a41cb
Oran-123 <orandillon123@gmail.com>
2023-01-11 17:38:58 +0000

    update the variable openModalButton

--- assets/js/howtoplaymodal.js
+++ assets/js/howtoplaymodal.js
@@ -1,4 +1,4 @@
-const openModalButton = document.getElementById("btn--how-to-play")
+const openModalButton = document.getElementById("btn-how-to-play")
 const closeModalButton = document.getElementById("button-close-rules-modal")
 const rulesModal = document.getElementById("rules-modal")
 
